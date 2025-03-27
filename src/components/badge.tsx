d927f553873b08f3320eab0875277cc6dd5b5e34
Griffin Westerman <gxweste@gmail.com>
2025-03-27 14:09:00 -0500

    chore: slightly dimmed badge text color

--- src/components/badge.tsx
+++ src/components/badge.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Badge({ children }: { children: React.ReactNode }) {
   return (
-    <div className="text-sm bg-secondary rounded-sm flex items-center py-1 px-2 text-foreground/80">
+    <div className="text-sm bg-secondary rounded-sm flex items-center py-1 px-2 text-foreground/70">
       {children}
     </div>
   )
